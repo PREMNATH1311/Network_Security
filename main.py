bf34e5eac2fb075b9b64580ed24e600880bbddec
unknown love <premnath97100@gmail.com>
2026-01-02 07:46:32 +0530

    updated data transfromation part pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.logging.logger import logging
 from networksecurity.exception.exception import NetworkSecurityException
 import sys
@@ -21,7 +22,12 @@ if __name__=="__main__":
         logging.info("Inititate the data validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         print(data_validation_artifact)
-        
+        logging.info("stated the  the data transformation")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info("initiate the data transformation")
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
